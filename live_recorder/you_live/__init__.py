8c1f62a39d5e21ef507b38ccfb6e7f8063bd3971
root <lijia0732@sina.com>
2021-01-29 13:47:56 +0800

    修复非开发环境无法自动加载recorders的bug

--- live_recorder/you_live/__init__.py
+++ live_recorder/you_live/__init__.py
@@ -1,20 +1,13 @@
-import pkgutil
-import sys
-module = sys.modules[__name__]
-
-for filefiner, name, ispkg in pkgutil.iter_modules(["you_live."], ""):
-    if not ispkg and not name.startswith("_") and name.endswith("_recorder"):
-        __import__(__name__ + "." + name)
-        try:
-            for attr in dir(getattr(module, name)):
-                if attr.endswith("Recorder") and not 'BaseRecorder' == attr:
-                    importCmd = "from .%s import %s"%(name, attr)
-                    #print(importCmd)
-                    exec(importCmd)
-        except:
-            break
-
 from .flv_checker import Flv
 from .live_thread.download import DownloadThread
 from .live_thread.monitoring import MonitoringThread
 from . import _recorder as Recorder
+import pkgutil
+import inspect
+import os
+
+filepath, tmpfilename = os.path.split(inspect.getfile(Recorder))
+
+for filefiner, name, ispkg in pkgutil.iter_modules([filepath]):
+    if not ispkg and not name.startswith("_") and name.endswith("_recorder"):
+        __import__(__name__ + "." + name)
