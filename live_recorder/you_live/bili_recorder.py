3917007cdcba89d738e142c531d3317780baa97e
lijia <lijia0732@sina.com>
2021-04-03 13:14:51 +0800

    fix #6 修复B站录制失败

--- live_recorder/you_live/bili_recorder.py
+++ live_recorder/you_live/bili_recorder.py
@@ -16,7 +16,7 @@ class BiliRecorder(BaseRecorder):
         url = "https://api.live.bilibili.com/room/v1/Room/get_info?id=%s&from=room"%self.short_id
         headers = {
             'Accept': 'application/json, text/plain, */*',
-            'Accept-Encoding': 'gzip, deflate, br',
+            #'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
             'Origin': 'https://live.bilibili.com',
             'Referer': 'https://live.bilibili.com/blanc/%s'%self.short_id,
